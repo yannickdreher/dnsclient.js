de67f9070da5cf37f4070ba15461516f41806938
Yannick Dreher <yannick.dreher@ifm.com>
2025-02-06 14:25:19 +0100

    updated header and minified file

--- src/dnsclient.js
+++ src/dnsclient.js
@@ -4,7 +4,7 @@
  * Author:   Yannick Dreher (yannick.dreher@dremaxx.de)
  * -----
  * Created:  Friday, 29th November 2024 3:30:10 pm
- * Modified: Tuesday, 4th February 2025 9:25:28 pm
+ * Modified: Thursday, 6th February 2025 2:24:43 pm
  * -----
  * License: MIT License (https://opensource.org/licenses/MIT)
  * Copyright © 2024-2025 Yannick Dreher
